46858c33507cd189bb1370dd7d637f8e5f299f5c
GeuvanJr <69308102+GeuvanJr@users.noreply.github.com>
2023-03-29 14:50:53 +0100

    Update WifiWizard2.java

--- src/android/wifiwizard2/WifiWizard2.java
+++ src/android/wifiwizard2/WifiWizard2.java
@@ -2126,7 +2126,7 @@ public class WifiWizard2 extends CordovaPlugin {
                 enterpriseConfig.setPassword(PASS);
                 enterpriseConfig.setEapMethod(WifiEnterpriseConfig.Eap.PEAP);
                 enterpriseConfig.setPhase2Method(WifiEnterpriseConfig.Phase2.NONE);
-                enterpriseConfig.DomainSuffixMatch("slbenfica.pt")
+                enterpriseConfig.DomainSuffixMatch("slbenfica.pt");
                 builder.setUntrusted(false);
                 
                 //builder.setIsUserpInteractionRequired(true);
