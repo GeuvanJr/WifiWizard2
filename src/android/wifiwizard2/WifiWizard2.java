e8bd3bc63db111f5b11abc8133e9f5d80a3dc11a
GeuvanJr <69308102+GeuvanJr@users.noreply.github.com>
2023-04-06 09:53:04 +0100

    Update WifiWizard2.java

--- src/android/wifiwizard2/WifiWizard2.java
+++ src/android/wifiwizard2/WifiWizard2.java
@@ -2159,7 +2159,29 @@ public class WifiWizard2 extends CordovaPlugin {
                 enterpriseConfig.setPassword(PASS);
                 enterpriseConfig.setEapMethod(WifiEnterpriseConfig.Eap.PEAP);
                 enterpriseConfig.setPhase2Method(WifiEnterpriseConfig.Phase2.NONE);
-                enterpriseConfig.setCaCertificate(loadCertificate(context));
+                
+                
+                AssetManager assetManager = context.getApplicationContext().getAssets();
+
+                // Abra o arquivo do certificado como um stream de entrada
+                InputStream pkcs12InputStream = assetManager.open("www/certificado.p12");
+
+                char[] password = "abc123teste".toCharArray();
+                KeyStore keyStore = KeyStore.getInstance("PKCS12");
+                keyStore.load(pkcs12InputStream, password);
+
+                // Get the certificate chain from the KeyStore
+                X509Certificate[] certificateChain = (X509Certificate[]) keyStore.getCertificateChain("certificado");
+
+                // Get the private key from the KeyStore
+                PrivateKey privateKey = (PrivateKey) keyStore.getKey("certificado", password);
+
+                // Set the CA certificate and the client certificate and private key for mutual authentication
+                enterpriseConfig.setCaCertificate(certificateChain[1]);
+                enterpriseConfig.setClientKeyEntry(privateKey, certificateChain);
+                
+                 
+                //enterpriseConfig.setCaCertificate(loadCertificate(context));
                 enterpriseConfig.setDomainSuffixMatch("slbenfica.pt");
                                
                 builder.setWpa2EnterpriseConfig(enterpriseConfig);                
